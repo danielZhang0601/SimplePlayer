44861ce278599b476fba01937e506b511f603f2f
danielZhang0601 <daniel.zhang@wolaidai.com>
2015-08-10 19:09:23 +0800

    jni cpp

--- jni/NativePlayer/NativePlayer_jni.cpp
+++ jni/NativePlayer/NativePlayer_jni.cpp
@@ -5,6 +5,48 @@
  *      Author: danielzhang
  */
 
+#include "com_zxd_simpleplayer_NativePlayer.h"
 
+JNIEXPORT void JNICALL Java_com_zxd_simpleplayer_NativePlayer_nativeInit(
+		JNIEnv *env, jobject clazz) {
 
+}
 
+JNIEXPORT void JNICALL Java_com_zxd_simpleplayer_NativePlayer_nativeSetDataSource(
+		JNIEnv *env, jobject clazz, jstring path) {
+
+}
+
+JNIEXPORT void JNICALL Java_com_zxd_simpleplayer_NativePlayer_nativeStart(
+		JNIEnv *env, jobject clazz) {
+
+}
+
+JNIEXPORT void JNICALL Java_com_zxd_simpleplayer_NativePlayer_nativePause(
+		JNIEnv *env, jobject clazz) {
+
+}
+
+JNIEXPORT void JNICALL Java_com_zxd_simpleplayer_NativePlayer_nativeStop(
+		JNIEnv *env, jobject clazz) {
+
+}
+
+JNIEXPORT void JNICALL Java_com_zxd_simpleplayer_NativePlayer_nativeSetPlaySpeed(
+		JNIEnv *env, jobject clazz, jint speed) {
+
+}
+
+JNIEXPORT void JNICALL Java_com_zxd_simpleplayer_NativePlayer_nativeSurfaceCreated(
+		JNIEnv *env, jobject clazz) {
+
+}
+
+JNIEXPORT jboolean JNICALL Java_com_zxd_simpleplayer_NativePlayer_nativeSurfaceChanged(
+		JNIEnv *env, jobject clazz, jint width, jint height) {
+	return true;
+}
+
+JNIEXPORT void JNICALL Java_com_zxd_simpleplayer_NativePlayer_nativeDrawFrame(
+		JNIEnv *env, jobject clazz) {
+}
