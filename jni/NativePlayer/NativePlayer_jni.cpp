78a54be66056916716f95450001d113ae2d8710d
danielZhang0601 <daniel.zhang@wolaidai.com>
2015-08-10 22:02:57 +0800

    add jni call

--- jni/NativePlayer/NativePlayer_jni.cpp
+++ jni/NativePlayer/NativePlayer_jni.cpp
@@ -1,20 +1,22 @@
 /*
  * NativePlayer_jni.cpp
  *
- *  Created on: 2015年8月10日
+ *  Created on: 2015骞�8鏈�10鏃�
  *      Author: danielzhang
  */
 
 #include "com_zxd_simpleplayer_NativePlayer.h"
+#include "PlayerCore.h"
+
+PlayerCore player;
 
 JNIEXPORT void JNICALL Java_com_zxd_simpleplayer_NativePlayer_nativeInit(
 		JNIEnv *env, jobject clazz) {
-
 }
 
 JNIEXPORT void JNICALL Java_com_zxd_simpleplayer_NativePlayer_nativeSetDataSource(
 		JNIEnv *env, jobject clazz, jstring path) {
-
+	player.init(env->GetStringUTFChars(path,false));
 }
 
 JNIEXPORT void JNICALL Java_com_zxd_simpleplayer_NativePlayer_nativeStart(
